1a8ddded4fad3b9601e15c8284d043c611120a84
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-05-17 01:16:07 -0700

    Create percectron.py

---
+++ percectron.py
@@ -0,0 +1,11 @@
+numpy
+def sigmoid(x):
+  return 1?( 1= np.exp(-x))
+
+def sigmoid_derivitive(x):
+  retun x * (1 - x)
+
+training _inputs = np. array([[0,0,1],
+                              [1,1,1],
+                               [1,0,1],
+                               [1,0,1,]])
