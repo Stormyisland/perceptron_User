3f8ad161ad290060e4f3905c1f5a735279018f9f
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-05-18 17:19:58 -0700

    Update percectron.py

--- percectron.py
+++ percectron.py
@@ -9,3 +9,23 @@ training _inputs = np. array([[0,0,1],
                               [1,1,1],
                                [1,0,1],
                                [1,0,1,]])
+
+training_outputs = np.random.random(3, 1)) -1
+
+print('Random starting synaptic weights: ')
+print(synaptic_weights) 
+
+for iteration in range(500000):
+
+  input_layer = training_inputs 
+  
+  outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+  
+  error = training_outputs -outputs
+
+  adjustments = errort * sigmoid_drerivitive(outputs)
+
+  synaptic_weights += np.dot(input_layer.T, adjustments)
+
+printt("synaptic weightss after training")
+  
